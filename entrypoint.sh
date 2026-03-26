51875ba8273d2d1f4e26b96970130ab75eebfd7a
Anton Sakhanovych <dev@antonsakhanovych.com>
2026-03-25 23:49:19 -0400

    Switch to production

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,4 +14,4 @@ sed \
   "${TPL}" > "${OUT}"
 
 echo "Realm JSON written from template."
-exec /opt/keycloak/bin/kc.sh start-dev --import-realm --http-port="${PORT:-8080}"
+exec /opt/keycloak/bin/kc.sh start --import-realm --http-port="${PORT:-8080}"
